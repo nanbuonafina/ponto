e6a5f0772c7b048bc6899662c2a986f364e64058
Fernanda Buonafina <tsarsc@gmail.com>
2026-05-16 05:00:39 +0000

    fix: corrigindo o arquivo SecurityConfig.java

--- src/main/java/com/pontoeletronico/backend/config/SecurityConfig.java
+++ src/main/java/com/pontoeletronico/backend/config/SecurityConfig.java
@@ -2,14 +2,21 @@ package com.pontoeletronico.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
@@ -17,5 +24,10 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
